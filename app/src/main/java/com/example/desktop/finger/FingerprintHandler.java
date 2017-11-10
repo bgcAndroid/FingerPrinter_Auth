6e8aaf3cf6d9309c0831c19a05f2b6500a77bf9f
Desktop <bgcniteen@gmail.com>
2017-11-10 13:05:21 +0530

    initial commit

--- app/src/main/java/com/example/desktop/finger/FingerprintHandler.java
+++ app/src/main/java/com/example/desktop/finger/FingerprintHandler.java
@@ -3,6 +3,7 @@ package com.example.desktop.finger;
 import android.content.Context;
 import android.hardware.fingerprint.FingerprintManager;
 import android.os.CancellationSignal;
+import android.util.Log;
 
 /**
  * Created by Desktop on 11/6/2017.
@@ -23,5 +24,11 @@ public class FingerprintHandler extends FingerprintManager.AuthenticationCallbac
     @Override
     public void onAuthenticationError(int errorCode, CharSequence errString) {
         super.onAuthenticationError(errorCode, errString);
+    }
+
+    @Override
+    public void onAuthenticationSucceeded(FingerprintManager.AuthenticationResult result) {
+        super.onAuthenticationSucceeded(result);
+        Log.i("Sucess", "onAuthenticationSucceeded:yes ");
     }
 }
